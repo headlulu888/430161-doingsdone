004e1f65dfd312ce8da23c5615b43fda63248bb7
headlulu <headlulu888@gmail.com>
2017-09-21 20:57:47 +0300

    Получение данных из формы:фикс 4

--- templates/form.php
+++ templates/form.php
@@ -16,24 +16,27 @@ $projects = ["Все", "Входящие", "Учеба", "Работа", "Дом
     <div class="form__row">
       <label class="form__label" for="name">Название <sup>*</sup></label>
 
-      <input class="form__input <?= $errors['title'] ?>" type="text" name="title" id="name" value="<?= $title ?>" placeholder="Введите название">
+      <input class="form__input <?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['title'] == '') print('form__input--error'); ?>" type="text" name="title" id="name" value="<?= $title ?>" placeholder="Введите название">
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['data'] == '') print("<span class='form__error'>Зaполните поле</span>")?>
     </div>
 
     <div class="form__row">
       <label class="form__label" for="project">Проект <sup>*</sup></label>
 
-      <select class="form__input form__input--select <?= $errors['project'] ?>" name="project" id="project" <?= $project ?>>
-        <option value=""></option>
+      <select class="form__input form__input--select <?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['project'] == '') print('form__input--error'); ?>" name="project" id="project">
+        <option value="<?= $project; ?>" placeholder="Введите проект"></option>
       <?php foreach ($projects as $key): ?>
         <option value="<?= $key; ?>"><?= $key; ?></option>
       <?php endforeach; ?>
       </select>
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['data'] == '') print("<span class='form__error'>Зaполните поле</span>")?>
     </div>
 
     <div class="form__row">
       <label class="form__label" for="date">Дата выполнения <sup>*</sup></label>
 
-      <input class="form__input form__input--date <?= $errors['date'] ?>" type="text" name="date" id="date" value="<?= $date ?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+      <input class="form__input form__input--date <?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['data'] == '') print('form__input--error'); ?>" type="text" name="date" id="date" value="<?= $date ?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+        <?php if($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['data'] == '') print("<span class='form__error'>Зaполните поле</span>")?>
     </div>
 
     <div class="form__row">
